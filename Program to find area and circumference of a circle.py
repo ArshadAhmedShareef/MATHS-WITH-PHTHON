1556f0ac2ea795135a409ee3e007d1c166c8b3b0
ArshadAhmedShareef <76745534+ArshadAhmedShareef@users.noreply.github.com>
2021-07-02 02:03:03 +0530

    Create Program to find area and circumference of a circle.py

---
+++ Program to find area and circumference of a circle.py
@@ -0,0 +1,6 @@
+#Program to find  area and circumference of a circle
+a=input("Enter radius of a circle")
+r=int(a)
+b=3.14*r**2
+c=2*3.14*r
+print("Area and circumference of a circle is",b,"and",c)
