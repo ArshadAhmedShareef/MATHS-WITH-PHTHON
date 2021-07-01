463de348aaf5adf2d45f4f25fcbee1d87667ae2a
ArshadAhmedShareef <76745534+ArshadAhmedShareef@users.noreply.github.com>
2021-07-02 02:12:08 +0530

    Create  Problem 4 A Program to find simple intrest when principle amount ,time,rate of intrest.py

---
+++  Problem 4 A Program to find simple intrest when principle amount ,time,rate of intrest.py
@@ -0,0 +1,6 @@
+ #A Program to find simple intrest when principle amount ,time,rate of intrest i
+p=int(input("Enter principal amount  : "))
+r=int(input("Enter tour interest rate :"))
+t=int(input("Enter time(in years)    : "))
+a=(p*(1+r*t))
+print( "SIMPLE INTEREST is",a )
