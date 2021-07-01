8c89a6d628c7f53271edcc0bad0ee534b3ab46eb
ArshadAhmedShareef <76745534+ArshadAhmedShareef@users.noreply.github.com>
2021-07-02 02:10:06 +0530

    Create # Problem 3 A Program to grade  students based upon following GPA.py

---
+++ # Problem 3 A Program to grade  students based upon following GPA.py
@@ -0,0 +1,8 @@
+#A Program to grade  students based upon following GPA
+a=float(input("Enter your GPA"))
+if (a==7.5):
+  print("A")
+if (a<7.5 and a>6.0):
+    print("B")
+if (a<6.0):
+    print("Fail")
